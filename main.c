4948e9c55a8f0509627bc6c9fe41091e10ddd47a
gustavomenezesh <45804139+gustavomenezesh@users.noreply.github.com>
2019-10-31 02:40:08 -0300

    main.c

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
-
+#include <string.h>
 #include "heap.h"
 
-int main(){
+int main(int argc, char* argv[]){
 
   struct binary_heap A;
   int i;
+  char diretory[40] = "instancias-num/";
+  strcat(diretory, argv[1]);	
 
+  printf("%s\n", diretory);
   FILE *f;
-  f = fopen("couting.txt", "r");
+  f = fopen(diretory, "r");
 
   fscanf(f, "%d", &A.length);
   new_heap(&A, A.length);
@@ -16,7 +19,7 @@ int main(){
     fscanf(f, "%d", &A.arr[i]);
 
   heapsort(&A);
-  	
+
   for(i = 0; i < A.length; i++)
   	printf("%d ", A.arr[i]);
   printf("\n");
